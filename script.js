1a4ea99de4602bee7e0cc2ce35764caa4506da39
unknown <adilkuyyo@gmail.com>
2020-07-21 21:38:01 +0530

    simple-calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+function dis(val) {
+	document.getElementById('display').value+=val;
+}
+function solve() {
+	var x=document.getElementById('display').value;
+	var y=eval(x);
+	document.getElementById('display').value=y;
+}
+function clr() {
+	document.getElementById('display').value="";
+}
